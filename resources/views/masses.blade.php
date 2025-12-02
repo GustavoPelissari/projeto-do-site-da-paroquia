5f902495056a1825804bd724c2f714c794ec2253
GustavoPelissari <gustavo12cristina@gmail.com>
2025-12-02 09:35:32 -0300

    Atualizar iframe do Google Maps com localização correta da Paróquia São Paulo Apóstolo

--- resources/views/masses.blade.php
+++ resources/views/masses.blade.php
@@ -247,11 +247,11 @@
                         </div>
                         
                         <div class="col-lg-6">
-                            <div class="rounded overflow-hidden" style="height: 100%; min-height: 350px;">
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14548.62969641595!2d-53.2972824!3d-23.7590074!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94f0f18d19ab85cd%3A0xd94e5f80df8017ab!2zUGFyw7NxdWlhIFNhbnRhIEplYW5lIGQnQXJj!5e0!3m2!1spt-BR!2sbr!4v1737564043558!5m2!1spt-BR!2sbr" 
+                            <div class="rounded overflow-hidden" style="height: 100%; min-height: 450px;">
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3651.9320702009004!2d-53.31768122466404!3d-23.749801678670803!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94f2d5905b80e317%3A0xbd22504d85d601ad!2zUGFyw7NxdWlhIFPDo28gUGF1bG8gQXDDs3N0b2xv!5e0!3m2!1spt-BR!2sbr!4v1764678632305!5m2!1spt-BR!2sbr" 
                                         width="100%" 
                                         height="100%" 
-                                        style="border:0; min-height: 350px;" 
+                                        style="border:0; min-height: 450px;" 
                                         allowfullscreen="" 
                                         loading="lazy" 
                                         referrerpolicy="no-referrer-when-downgrade">
